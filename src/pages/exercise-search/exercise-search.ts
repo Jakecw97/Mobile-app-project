23288b31a89760f8a3cfab0b1e3bda9b722cf447
Jakecw97 <g00349377@gmit.ie>
2018-05-04 17:19:33 +0100

    Style update

--- src/pages/exercise-search/exercise-search.ts
+++ src/pages/exercise-search/exercise-search.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { InAppBrowser, InAppBrowserOptions } from '@ionic-native/in-app-browser';
+import { TabsPage } from '../tabs/tabs';
 
 /**
  * Generated class for the ExerciseSearchPage page.
@@ -19,9 +20,10 @@ export class ExerciseSearchPage {
   constructor(public navCtrl: NavController, public navParams: NavParams, private inAppBrowser: InAppBrowser) {
   }
 
-  ionViewDidEnter() { //Refreshes the browser each time the page is entered
+  ionViewWillEnter() { //Refreshes the browser each time the page is about to be entered
     console.log('ionViewDidLoad ExerciseSearchPage');
     this.openWebPage()
+  
   }
 
   openWebPage(){
@@ -29,10 +31,13 @@ export class ExerciseSearchPage {
       zoom: 'yes'
     }
     const browser = this.inAppBrowser.create('https://www.bodybuilding.com/exercises', '_self', options); // _self ensures the search site opens in the inappbrowser
- 
-    
     
   }
-
+  
+  ionViewDidEnter() { //pushes back to tabs page upon browser
+    
+    this.navCtrl.push(TabsPage)
+  
+  }
 
 }
